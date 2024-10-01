64bb1df89d4a6e839ca4e97c0b7d563ab4b79c16
monger18 <satyamsri1809@gmail.com>
2024-10-02 04:59:34 +0530

    Updated Readme

--- backend/middleware/admin.js
+++ backend/middleware/admin.js
@@ -1,6 +1,6 @@
 export const isAdmin = (req, res, next) => {
   const role = req.user.role
-  console.log({ role })
+  //console.log({ role })
 
   if (req.user.role !== 'admin') {
     return res.status(404).json({ error: 'Forbidden: Access denied' })
